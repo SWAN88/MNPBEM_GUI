68063c3ebffd6c40f36ef4dc083ab841e6614b07
Katsuya Shiratori <kyo.soc.einstein88@gmail.com>
2024-11-04 15:04:22 -0600

    update

--- MNPBEM17/Examples/far_field/Yes_Substrate_Yes_CTAB/Scat_Abs_Rod_CTAB_Sub.m
+++ MNPBEM17/Examples/far_field/Yes_Substrate_Yes_CTAB/Scat_Abs_Rod_CTAB_Sub.m
@@ -7,7 +7,7 @@ addpath(genpath('C:\Users\katsuya2\OneDrive - University of Illinois - Urbana\Do
 %% options for BEM simulation
 
 % set dielectric environment
-epstab = {epsconst(1), epstable('gold.dat'), epsconst(1.52^2), epsconst(1.44^2)};
+epstab = {epsconst(1), epstable('gold_olmon.dat'), epsconst(1.52^2), epsconst(1.44^2)};
 
 ztab = 0;
 op = layerstructure.options;
@@ -15,13 +15,17 @@ layer = layerstructure(epstab, [1, 3], ztab, op);
 op = bemoptions('sim', 'ret', 'interp', 'curv', 'waitbar', 0, 'layer', layer);
 
 % initialize nanorod
-length_rod = 60;
 width_rod = 20;
-core = trirod(width_rod, length_rod, [20, 10, length_rod / 2]);
+length_rod = 60;
+nphi = 3;
+ntheta = 3;
+nz = 3;
+
+core = trirod(width_rod, length_rod, [(width_rod+1)*(pi/nphi), (width_rod+1)/ntheta, (length_rod-width_rod+1)/nz], 'triangles');
 
 % initialize shell
 shellthickness = 3.5;
-shell = trirod(width_rod+shellthickness*2, length_rod+shellthickness*2, [20, 10, length_rod / 2], 'triangles');
+shell = trirod(width_rod+shellthickness*2, length_rod+shellthickness*2, [(width_rod+1)*(pi/nphi), (width_rod+1)/ntheta, (length_rod-width_rod+1)/nz], 'triangles');
 % array of dielectrics with first row as [in,out] for core and second row as [in,out] for shell, eg. [1,2;2,3]
 p = comparticle(epstab, {core, shell}, [2, 4; 4, 1], 1, 2, op); 
 p = rot(p, 90, [0, 1, 0]);
@@ -30,7 +34,7 @@ p = rot(p, 90, [0, 1, 0]);
 figure(1)
 plot(p, 'EdgeColor', 'b');
 
-enei = 400:10:1000;
+enei = 400:100:1000;
 
 % 
 if ~exist('greentab', 'var') || ~greentab.ismember(layer, enei, p)
@@ -52,7 +56,7 @@ exc = planewave([1, 0, 0], [0, 0, 1], op);
 sca = zeros(length(enei), 2);
 ext = zeros(length(enei), 2);
 multiWaitbar('BEM solver', 0, 'Color', 'g', 'CanCancel', 'on');
-%  loop over wavelengths
+% loop over wavelengths
 for ien = 1 : length(enei)
   % surface charge
   sig = bem \ exc(p, enei(ien));
@@ -64,8 +68,6 @@ end
 multiWaitbar('CloseAll');
 
 %% final plots
-particle_name = ['length_' num2str(length_rod) '_width_' num2str(width_rod)];
-
 fig1 = figure('visible','on');
 abs = ext - sca;
 plot(enei, sum(abs, 2), '-','LineWidth', 1);  
@@ -77,17 +79,25 @@ plot(enei, sum(sca, 2), '-', 'LineWidth', 1);
 xlabel('Wavelength (nm)', 'FontSize', 20);
 ylabel('Scattering cross section (nm^2)', 'FontSize', 20);
 
-% %% save data
-% filepath = "C:\Users\katsuya2\OneDrive - University of Illinois - Urbana\Documents\MATLAB\MNPBEM_GUI\MNPBEM17\Examples\far_field\Yes_Substrate_Yes_CTAB";
-% 
-% abs_file = [enei', abs, sum(abs, 2)];
-% sca_file = [enei', sca, sum(sca, 2)];
-% 
-% % Define file paths with the desired directory and filenames
-% abs_filepath = fullfile(filepath, [particle_name '_abs.txt']);
-% sca_filepath = fullfile(filepath, [particle_name '_sca.txt']);
-% 
-% % Save data
-% save(abs_filepath, 'abs_file', '-ascii');
-% save(sca_filepath, 'sca_file', '-ascii');
+%% save data
+particle_name = ['length_' num2str(length_rod) '_width_' num2str(width_rod)];
+filepath = "C:\Users\katsuya2\OneDrive - University of Illinois - Urbana\Documents\MATLAB\MNPBEM_GUI\MNPBEM17\Examples\far_field\Yes_Substrate_Yes_CTAB";
+
+spec_file = [enei', sum(sca, 2)*1e-18, sum(abs, 2)*1e-18];
+
+% Define file paths with the desired directory and filenames
+spec_filepath = fullfile(filepath, [particle_name 'ctab_sub_spec.csv']);
+
+% Open the file for writing
+fid = fopen(spec_filepath, 'w');
+
+% Write the header
+fprintf(fid, 'wav,scat,abs');
+
+% Close and save data
+fclose(fid);
+
+% Save data as a CSV file
+writematrix(spec_file, spec_filepath, 'WriteMode', 'append');
+
 
